9d5ff145613fe8cdba9b6c724cb276a92822f25d
Mudambaelvis <you@example.com>
2026-04-12 18:32:47 +0300

    mail transporter port

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,10 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS // Use the App Password from Google
+        pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        // This helps if the server has trouble verifying certificates
+        rejectUnauthorized: false 
     }
 });
 
