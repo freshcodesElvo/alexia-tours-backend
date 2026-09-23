f215a7514b6e59d89b6a6ebdb7c5f2bed07015d8
freshcodesElvo <elvismudamba@gmail.com>
2026-09-23 20:26:01 +0300

    Allow Next.js development origin

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const allowedOrigins = [
     "https://alexiastours.co.ke",
     "https://www.alexiastours.co.ke",
     "https://freshcodeselvo.github.io",
+
+    // Next.js development
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
+    // Existing development origins
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://127.0.0.1:5501",
